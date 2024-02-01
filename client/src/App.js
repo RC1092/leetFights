0d4c8ce97a79397f193f292dfad958f9189aefe0
Mirza Yousuf <myzabrocks@gmail.com>
2024-02-01 13:12:58 -0330

    Only back end works , have to bring data to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useEffect ,useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Sidebar from './sideBar';
 import Topbar from './topBar';
+
+
+
 //import Home from './Home';
 
 //import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  const [backEndData,setBackEndData] = useState([{}])
+
+
+    useEffect(()=>{
+      fetch("/check",{
+        method:"GET",
+        credentials:"include",
+      }).then(
+        response => response.json()
+      ).then(
+        data=>{
+          setBackEndData(data)
+        }
+      ).catch(e => {
+        console.log(e);
+    });
+    },[])
+    
+  
+
+  console.log(backEndData)
+
   return (
     <Router>
     <div className="app">
         <Topbar />
         <Sidebar />
         <Routes>
-        <Route path="/" exact component={<div></div>} />
+        <Route path="/" exact element={<div></div>} />
 
         </Routes>
         {/* Add more routes as needed */}
